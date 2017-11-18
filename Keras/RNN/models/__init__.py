6e5e43e2846f292f1c4db085cbba6e539be12950
slowmoyang <slowmoyang@gmail.com>
2017-11-18 20:49:15 +0900

    added models dir

---
+++ Keras/RNN/models/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import models.model_ak4
+
+__all__ = ["model_ak4"]
+
+def build_a_model(model_name, *args, **kargs):
+    return getattr(models, model_name).build_a_graph(*args, **kargs)
